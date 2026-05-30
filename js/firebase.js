f09ce8470175b70c5c587faa394f4dc942ec0416
sayedsharaf2025-lang <sayedsharaf2025@gmail.com>
2026-05-31 02:04:42 +0300

    Add files via upload

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,14 @@
 /* ====== Firebase Configuration ====== */
-https://sayedsharaf2025-lang.github.io/fathysabermob/#
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
